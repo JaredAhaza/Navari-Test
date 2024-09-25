e60e51b4034adde1c60ed91347c946773357562f
JaredAhaza <jaredahazq@gmail.com>
2024-09-25 19:11:44 +0300

    Updated my admin file to allow it to list more items, implemented the django search method for the admin file and imlemented the filter method

--- library/admin.py
+++ library/admin.py
@@ -8,18 +8,24 @@ import pdfkit
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'price')
+    list_display = ('title', 'author', 'category', 'price', 'is_available', 'borrowed_by')
+    search_fields = ('title', 'author')
+    list_filter = ('author', 'category', 'is_available', 'borrowed_by')
 admin.site.register(Book, BookAdmin)
-
 class DebtAdmin(admin.ModelAdmin):
-    pass    
+    list_display = ('customer', 'total_debt')
+    search_fields = ('customer', 'total_debt')
+    list_filter = ('total_debt', 'customer')    
 admin.site.register(Debt, DebtAdmin)
 class BorrowingAdmin(admin.ModelAdmin):
-    pass    
+    list_display = ('book', 'customer', 'issue_date', 'book_price')
+    search_fields = ('book', 'customer')
+    list_filter = ('issue_date', 'customer')
 admin.site.register(Borrowing, BorrowingAdmin)
-
 class ReturningAdmin(admin.ModelAdmin):
-    pass    
+    list_display = ('borrowing', 'is_returned', 'is_late', 'is_damaged', 'fine')
+    search_fields = ('borrowing', 'is_returned', 'is_late', 'is_damaged')
+    list_filter = ('is_returned', 'is_late', 'is_damaged')
 admin.site.register(Returning, ReturningAdmin)
 
 def download_invoice_pdf(modeladmin, request, queryset):
@@ -39,5 +45,7 @@ def mark_as_paid(modeladmin, request, queryset):
 mark_as_paid.short_description = "Mark selected invoices as paid"
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = ('customer', 'date_generated', 'total_amount', 'is_paid')
+    search_fields = ('customer', 'date_generated')
+    list_filter = ('date_generated', 'is_paid')
     actions = [mark_as_paid,download_invoice_pdf]
 admin.site.register(Invoice, InvoiceAdmin)
