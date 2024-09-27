24b79590875a0485c1ce3cc2542ba7873c93f3aa
JaredAhaza <jaredahazq@gmail.com>
2024-09-27 09:33:44 +0300

    Updated settings in readiness for deployment

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -21,12 +21,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r+_nw4ng!9!5bz_p++&2y72*gus=z%2y&5r3_#mwdqe9txf=_^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['navari-test.herokuapp.com','*']
 
 
 # Application definition
