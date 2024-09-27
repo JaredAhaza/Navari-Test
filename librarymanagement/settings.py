3a1c6d6ce594e956c1213c7eec4b89c05bc3b451
JaredAhaza <jaredahazq@gmail.com>
2024-09-27 09:47:04 +0300

    Updated settings

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-r+_nw4ng!9!5bz_p++&2y72*gus=z%2y&5r3_#mwdqe9txf=_^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
